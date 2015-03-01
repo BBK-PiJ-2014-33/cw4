b9d1820ee75341f5721cb6fa37ee2596c4cf3850
BBK-PiJ-2014-33 <ludak303@gmail.com>
2015-03-01 09:46:01 +0000

    Contact class work
    Created ContactImpl class
    Created basic testing framework for ContactImpl class

---
+++ Contact/ContactImpl.java
@@ -0,0 +1,21 @@
+package Contact;
+
+public class ContactImpl implements Contact {
+
+    public int getId()
+    {
+
+    }
+    public String getName()
+    {
+
+    }
+    public String getNotes()
+    {
+
+    }
+    public void addNotes(String note)
+    {
+
+    }
+}
---
+++ Tests/ContactImplTest.java
@@ -0,0 +1,32 @@
+package Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContactImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNotes() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNotes() throws Exception {
+
+    }
+}
