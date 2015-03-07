355dc71735e0963ec80ca1e7aff5cb7a3fb93443
BBK-PiJ-2014-33 <ludak303@gmail.com>
2015-03-07 15:51:18 +0000

    Step 1 of TDD for ContactImpl
    Written tests for ContactImpl and made sure they have failed

--- ContactImplTest.java
+++ ContactImplTest.java
@@ -1,5 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
 
 public class ContactImplTest {
 
@@ -11,23 +13,63 @@ public class ContactImplTest {
         myContactClass = new ContactImpl();
     }
 
+    @After
+    public void cleanUp() throws Exception {
+        myContactClass = new ContactImpl();
+    }
+
     @Test
     public void testGetId() throws Exception {
-
+        assertNotNull(myContactClass.getId());
     }
 
     @Test
     public void testGetName() throws Exception {
-
+        assertNotNull(myContactClass.getName());
     }
 
     @Test
     public void testGetNotes() throws Exception {
-
+        assertNotNull(myContactClass.getNotes());
     }
 
     @Test
     public void testAddNotes() throws Exception {
+
+        String expected = "test";
+        String output;
+        myContactClass.addNotes("test");
+        output = myContactClass.getNotes();
+        assertEquals(output,expected);
+    }
+
+    @Test
+    public void testIdUnique() throws Exception
+    {
+
+        int numberOfContacts = 10;
+        int myId1, myId2;
+        Contact [] myContacts = new ContactImpl[numberOfContacts];
+
+        for (int i = 0; i < numberOfContacts; i++)
+        {
+            myContacts[i] = new ContactImpl();
+        }
+
+        for (int i = 0; i < numberOfContacts; i++)
+        {
+            myId1 = myContacts[i].getId();
+            for (int k = 0; k < numberOfContacts; k++)
+            {
+                if(i!=k)
+                {
+                    myId2 = myContacts[k].getId();
+                    assertNotEquals(myId1,myId2);
+                }
+
+            }
+
+        }
 
     }
 }
