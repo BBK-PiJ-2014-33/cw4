a5cb97fec4a482319bc50b15ea9aface7c5723f2
BBK-PiJ-2014-33 <ludak303@gmail.com>
2015-03-07 17:45:27 +0000

    Create MeetingImpl and MeetingImplTest

---
+++ MeetingImpl.java
@@ -0,0 +1,19 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class MeetingImpl implements Meeting {
+
+    int getId();
+    /**
+     * Return the date of the meeting. *
+     * @return the date of the meeting. */
+    Calendar getDate();
+    /**
+     * Return the details of people that attended the meeting. *
+     * The list contains a minimum of one contact (if there were
+     * just two people: the user and the contact) and may contain an
+     * arbitraty number of them. *
+     * @return the details of people that attended the meeting. */
+    Set<Contact> getContacts();
+}
+
---
+++ MeetingImplTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class MeetingImplTest {
+
+    private MeetingImpl myMeetingClass;
+
+    @Before
+    public void setUp() throws Exception {
+        myMeetingClass = new MeetingImpl();
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetContacts() throws Exception {
+
+    }
+}
