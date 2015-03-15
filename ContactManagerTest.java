49a739ca1999a3250afcaa373e5501d75a5ab0e7
unknown <lveshn01@CSPC118.dcs.bbk.ac.uk>
2015-03-15 09:57:58 +0000

    Step 1 of TDD addFutureMeeting
    added three tests and make sure they fail

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -1,18 +1,58 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class ContactManagerTest {
+    ContactManager myContactManagerClass;
+    Calendar myDate;
+    private Set<Contact> myContacts;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
+        int numberOfContacts = 5;
+        myDate = Calendar.getInstance();
+        myContacts = new HashSet<Contact>();
+        String [] Names = {"Anna Jones", "David Crampton", "Maria Garcia", "Nick White", "Scott Goldstone"};
+        for (int i = 0; i < numberOfContacts; i++)
+        {
+            myContacts.add(new ContactImpl(Names[i]));
+        }
 
     }
 
-    @Test
-    public void testAddFutureMeeting() throws Exception {
+    @Test(expected = IllegalArgumentException.class)
+    public void testFutureMeetingInPast()
+    {
+        Calendar myPastDate = Calendar.getInstance();
+        myPastDate.set(1900, Calendar.JANUARY, 30);
+        myContactManagerClass.addFutureMeeting(myContacts,myDate);
 
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testFutureMeetingIllegalContact()
+    {
+        Set<Contact> myFutureMeetingContacts;
+        myFutureMeetingContacts = new HashSet<Contact>();
+        int numberOfContacts = 5;
+        String [] Names = {"Anna Jones", "David Crampton", "Maria Garcia", "Nick White", "Scott Goldstone"};
+        for (int i = 0; i < numberOfContacts; i++)
+        {
+            myFutureMeetingContacts.add(new ContactImpl(Names[i]));
+        }
+        myFutureMeetingContacts.add(new ContactImpl("Emily Storey"));
+        myContactManagerClass.addFutureMeeting(myContacts,myDate);
+    }
+
+    @Test
+    public void testAddFutureMeetingNotNull() throws Exception
+    {
+        assertNotNull(myContactManagerClass.addFutureMeeting(myContacts,myDate));
     }
 
     @Test
