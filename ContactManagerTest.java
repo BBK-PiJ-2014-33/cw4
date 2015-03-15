f4bf2c5d51524d241e1f542a8fdbef8cc284ab29
unknown <lveshn01@CSPC118.dcs.bbk.ac.uk>
2015-03-15 08:34:25 +0000

    added ContactManagerTest

---
+++ ContactManagerTest.java
@@ -0,0 +1,77 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContactManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAddFutureMeeting() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPastMeeting() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFutureMeeting() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMeeting() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFutureMeetingList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFutureMeetingList1() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPastMeetingList() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNewPastMeeting() throws Exception {
+
+    }
+
+    @Test
+    public void testAddMeetingNotes() throws Exception {
+
+    }
+
+    @Test
+    public void testAddNewContact() throws Exception {
+
+    }
+
+    @Test
+    public void testGetContacts() throws Exception {
+
+    }
+
+    @Test
+    public void testGetContacts1() throws Exception {
+
+    }
+
+    @Test
+    public void testFlush() throws Exception {
+
+    }
+}
